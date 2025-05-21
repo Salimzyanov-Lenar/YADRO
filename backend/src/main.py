8af57b203acd82c85354746ba15284e1735bc872
Salimzyanov Lenar <salimzyanov_lenar@mail.ru>
2025-05-21 15:20:31 +0400

    fix: added a new cors urls

--- backend/src/main.py
+++ backend/src/main.py
@@ -38,7 +38,12 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://127.0.0.1:5173", "http://127.0.0.1:3000"],
+    allow_origins=[
+        "http://127.0.0.1:5173",
+        "http://127.0.0.1:3000",
+        "http://localhost:5173",
+        "http://localhost:3000",
+        ],
     allow_credentials=True,
     allow_methods=['GET'],
     allow_headers=['*'],
