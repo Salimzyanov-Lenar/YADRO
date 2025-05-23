c78d9cd4c9b861639a2aba964664f4ca095d43e0
Salimzyanov Lenar <salimzyanov_lenar@mail.ru>
2025-05-23 15:56:57 +0400

    new: try/except block for fetching users

--- backend/src/users/fetch_users.py
+++ backend/src/users/fetch_users.py
@@ -1,4 +1,7 @@
 import aiohttp
+import asyncio
+
+from pydantic import ValidationError
 from src.users.schemas import ExternalResponse
 from src.config import settings
 
@@ -8,7 +11,16 @@ API_URL = settings.API_URL
 
 async def fetch_users(amount: int) -> ExternalResponse:
     timeout = aiohttp.ClientTimeout(total=10)
-    async with aiohttp.ClientSession(timeout=timeout) as session:
-        async with session.get(API_URL, params={"results": amount}) as response:
-            data = await response.json()
-            return ExternalResponse(**data)
+    try:
+        async with aiohttp.ClientSession(timeout=timeout) as session:
+            async with session.get(API_URL, params={"results": amount}) as response:
+                data = await response.json()
+                return ExternalResponse(**data)
+    except (aiohttp.ClientError, asyncio.TimeoutError) as e:
+        print(f"Ошибка при запросе к внешнему API: {e}")
+    except ValidationError as e:
+        print(f"Ошибка валидации данных от API: {e}")
+    except Exception as e:
+        print(f"Непредвиденная ошибка: {e}")
+
+    return None
